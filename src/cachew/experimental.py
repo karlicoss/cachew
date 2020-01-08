c5ab135588b7319c49543f7006c6c4374b7b4992
Dima Gerasimov <karlicoss@gmail.com>
2020-01-08 21:27:31 +0000

    Optional feature: Exception support

---
+++ src/cachew/experimental.py
@@ -0,0 +1,35 @@
+from typing import Optional
+
+import sqlalchemy  # type: ignore
+
+from . import PRIMITIVES
+
+
+# NOTE ok, not ideal converting everything to str; but after all what else you can to with Exception??
+class ExceptionAdapter(sqlalchemy.TypeDecorator):
+    impl = sqlalchemy.String
+
+    @property
+    def python_type(self): return Exception
+
+    def process_literal_param(self, value, dialect): raise NotImplementedError()  # make pylint happy
+
+    def process_bind_param(self, value: Optional[Exception], dialect) -> Optional[str]:
+        if value is None:
+            return None
+        return str(value.args)
+
+    def process_result_value(self, value: Optional[str], dialect) -> Optional[Exception]:
+        if value is None:
+            return None
+        return Exception(value)
+
+
+def enable_exceptions():
+    if Exception not in PRIMITIVES:
+        PRIMITIVES[Exception] = ExceptionAdapter
+
+
+def disable_exceptions():
+    if Exception in PRIMITIVES:
+        del PRIMITIVES[Exception]
